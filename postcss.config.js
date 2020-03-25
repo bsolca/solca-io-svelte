6500bea74225ee435ce959b61ca976bed5468c75
Benjamin <benjamin@solca.io>
2020-03-25 22:49:44 +0100

    [+] postcss impl

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,23 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [
-        './**/**/*.html',
-        './**/**/*.svelte'
-    ],
+const postcssPlugins = (purgecss = false) => {
+    return [
+        require('postcss-import')(),
+        require('postcss-url')(),
+        require('tailwindcss')('./tailwind.config.js'),
+        require('autoprefixer')(),
+        purgecss &&
+        require('@fullhuman/postcss-purgecss')({
+            content: ['./src/**/*.svelte', './src/**/*.html'],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        }),
+        !dev && require('cssnano')({preset: 'default'}),
+    ].filter(Boolean);
+};
 
-    whitelistPatterns: [/svelte-/],
-
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
-
-const production = process.env.NODE_ENV !== 'development'
+const production = process.env.NODE_ENV !== 'development';
 
 module.exports = {
     plugins: [
         require('tailwindcss'),
-        ...(production ? [purgecss] : [])
+        ...(production ? [postcssPlugins] : [])
     ]
 };
