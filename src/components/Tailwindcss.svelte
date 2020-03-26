0c931424e4a49b6a94a8fb27ce424056073a6188
Benjamin <benjamin@solca.io>
2020-03-26 11:38:21 +0100

    [+] Allow for later custom

--- src/components/Tailwindcss.svelte
+++ src/components/Tailwindcss.svelte
@@ -1,5 +1,5 @@
 <style global>
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
+    @import 'tailwindcss/base';
+    @import 'tailwindcss/components';
+    @import 'tailwindcss/utilities';
 </style>
