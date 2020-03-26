d098ed24f26f8b0bc7e9a571132ad025f965ba8c
Benjamin <benjamin@solca.io>
2020-03-26 11:11:10 +0100

    [~] Modify indentation

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,30 @@
-const postcssPlugins = (purgecss = false) => {
+const production = process.env.NODE_ENV !== 'development';
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project
+    content: [
+        './src/**/*.svelte',
+        './src/**/*.html'
+    ],
+
+    whitelistPatterns: [/svelte-/],
+
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+const postcssPlugins = () => {
     return [
         require('postcss-import')(),
         require('postcss-url')(),
         require('tailwindcss')('./tailwind.config.js'),
         require('autoprefixer')(),
-        purgecss &&
-        require('@fullhuman/postcss-purgecss')({
-            content: ['./src/**/*.svelte', './src/**/*.html'],
-            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-        }),
+        purgecss,
         !dev && require('cssnano')({preset: 'default'}),
     ].filter(Boolean);
 };
 
-const production = process.env.NODE_ENV !== 'development';
 
 module.exports = {
     plugins: [
@@ -21,3 +32,5 @@ module.exports = {
         ...(production ? [postcssPlugins] : [])
     ]
 };
+
+console.log("Am I in prod? >> " + production);
