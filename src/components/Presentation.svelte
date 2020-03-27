e93195a11e22c12c6ae6cc36f38d05274740dfc5
Benjamin <benjamin@solca.io>
2020-03-27 16:15:00 +0100

    [+] Add basic global test style

--- src/components/Presentation.svelte
+++ src/components/Presentation.svelte
@@ -1,5 +1,15 @@
-<style>
+<script>
+    export let name = 'Benjamin';
+</script>
 
+<style>
+    section {
+        @apply bg-green-100 h-40 flex items-center justify-center flex-row;
+    }
+    h1 {
+    }
 </style>
 
-<h1>Hello my name is Benjamin!</h1>
+<section>
+    <h1>Hello my name is {name}!</h1>
+</section>
